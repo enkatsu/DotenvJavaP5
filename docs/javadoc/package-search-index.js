ecf2b3d481de6a27ccdbed79fde9768a52fa2d74
Katsuya Endoh <studio@enkatsu.org>
2024-07-02 02:40:05 +0900

    add docs

---
+++ docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"org.enkatsu.dotenvp5"},{"l":"すべてのパッケージ","u":"allpackages-index.html"}];updateSearchResults();
---
+++ docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.enkatsu.dotenvp5","l":"DotenvP5"}];updateSearchResults();
